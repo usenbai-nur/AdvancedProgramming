ae996b42c09a5ef6fabf08187e18770c45681466
nurbol amangeldinov <nurbolamangeldinov10@gmail.com>
2026-01-25 17:20:50 +0500

    Add ERD, UseCase diagrams and main.go by Nurbol

--- main.go
+++ main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/cars", carsPage)
+	http.HandleFunc("/orders", ordersPage)
+	http.HandleFunc("/auth", authPage)
+
+	fmt.Println("Car Store API - http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Car Store - Team: Nurdaulet, Nurbol, Ehson")
+}
+
+func carsPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Cars Module - Nurdaulet")
+}
+
+func ordersPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Orders Module - Nurbol")
+}
+
+func authPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Auth Module - Ehson")
+}
